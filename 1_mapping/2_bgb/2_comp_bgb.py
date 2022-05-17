13818aa345cd030d83a2d8e66eb8e4fe3874c809
srgorelik <sgorelik@whrc.org>
2022-05-17 21:27:52 +0000

    bgb adj

--- 1_mapping/2_bgb/2_comp_bgb.py
+++ 1_mapping/2_bgb/2_comp_bgb.py
@@ -3,7 +3,7 @@
 from raspy import *
 
 # root:shoot ratios
-f_r2s = 'Root2Shoot_Ratios_Scaled1e3.tif'
+f_r2s = '/home/sgorelik/data/pnas/Root2Shoot_Ratios_Scaled1e3_500m.tif'
 r2s = raster(f_r2s, verbose = True)
 
 # apply scaling factor
@@ -19,10 +19,10 @@ def comp_bgb(r2s, f_agb, f_bgb):
 	# multiply aboveground by root:shoot ratios
 	bgb_flt = agb * r2s
 	
-	# apply mokany et al.'s (2006) eq. 1 to pixels with missing bgb
-	index_eq1 = (agb != nd) & (agb > 0) & (bgb_flt == 0) & (r2s == 0)
-	bgb_flt[index_eq1] = np.power(agb[index_eq1], 0.89) * 0.489
-	del index_eq1
+	# apply mokany et al.'s (2006) eq. 1 to pixels with no r:s ratio
+	eq1_ind = (agb != nd) & (agb > 0) & (r2s == 0)
+	bgb_flt[eq1_ind] = np.power(agb[eq1_ind], 0.89) * 0.489
+	del eq1_ind
 	
 	bgb_int = np.rint(bgb_flt).astype(np.int16)
 	del bgb_flt
@@ -35,8 +35,8 @@ def comp_bgb(r2s, f_agb, f_bgb):
 
 def main():
 	
-	# aboveground
-	f_agb_lst = ['Current_AGB_Mgha_500m.tif', 'Potential_AGB_Mgha_500m.tif']
+	# aboveground inputs
+	f_agb_lst = ['/home/sgorelik/data/pnas/Current_AGB_Mgha_500m.tif', '/home/sgorelik/data/pnas/Potential_AGB_Mgha_500m.tif']
 	
 	# compute belowground
 	for f_agb in f_agb_lst:
