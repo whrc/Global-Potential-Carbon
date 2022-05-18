6c3cab77180d3a30eef2aa51a362fd96b9513d65
srgorelik <sgorelik@whrc.org>
2022-05-18 14:07:30 +0000

    minor fix

--- 1_mapping/6_constraints/7_apply_constraints.py
+++ 1_mapping/6_constraints/7_apply_constraints.py
@@ -14,5 +14,5 @@ gt, sr = get_gt_sr(f_cons)
 
 for pool in ['AGB', 'BGB', 'AGB_BGB', 'SOC', 'AGB_BGB_SOC']:
 	unr = raster('Base_Unr_{}_MgCha_500m.tif'.format(pool), verbose = True)
-	unr[cons > 0] = -32768
+	unr[cons > 0] = 0
 	write_gtiff(unr, 'Base_Con_Unr_{}_MgCha_500m.tif'.format(pool), dtype = 'Int16', gt = gt, sr = sr, nodata = -32768, stats = True, msg = True)
