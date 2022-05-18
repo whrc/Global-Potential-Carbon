c6481685d06b277a0dddf2cabc39b6f28bb7c2f0
srgorelik <sgorelik@whrc.org>
2022-05-18 01:44:39 +0000

    just mapping

--- 1_mapping/5_combine_pools/combine_pools.py
+++ 1_mapping/5_combine_pools/combine_pools.py
@@ -9,7 +9,6 @@ for store in stores:
 	
 	f_agb = '{}_AGB_MgCha_500m.tif'.format(store)
 	agb = raster(f_agb, verbose = True)
-	ha = get_cell_area_ha(f_agb)
 	gt, sr = get_gt_sr(f_agb)
 	
 	bgb = raster('{}_BGB_MgCha_500m.tif'.format(store), verbose = True)
@@ -22,14 +21,12 @@ for store in stores:
 	
 	# AGB+BGB only
 	bio = agb + bgb
-	print(round(np.sum(bio[bio > 0]*ha)/1e9, 1))
 	bio[ind] = nd
 	write_gtiff(bio, '{}_AGB_BGB_MgCha_500m.tif'.format(store), dtype = 'Int16', gt = gt, sr = sr, nodata = nd, stats = True, msg = True)
 	del bio
 	
 	# AGB+BGB+SOC
 	tot = agb + bgb + soc
-	print(round(np.sum(tot[tot > 0]*ha)/1e9, 1))
 	tot[ind] = nd
 	write_gtiff(tot, '{}_AGB_BGB_SOC_MgCha_500m.tif'.format(store), dtype = 'Int16', gt = gt, sr = sr, nodata = nd, stats = True, msg = True)
 	del tot
